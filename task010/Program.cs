9898a3e97922bf540af137afa05188ad2f7b82b8
Aleksey.V <aleksey.vahoninnd@gmail.com>
2023-07-24 15:00:00 +0300

    task010

---
+++ task010/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и 
+//на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.WriteLine("Введите 3-х значное число:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (ThreeDigit(number))
+{
+    int result = DigitSecond(number);
+    Console.WriteLine($"{number} -> {result}");
+}
+else
+{
+    Console.WriteLine("Число не 3-х значное");
+}
+
+int DigitSecond(int num)
+{
+    int digitSecond = (num / 10) % 10;
+    return digitSecond;
+}
+
+bool ThreeDigit(int num)
+{
+    return (num > 99 && num < 1000) || (num < -99 && num > -1000);
+}
