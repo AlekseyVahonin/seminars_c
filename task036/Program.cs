bdf2918038a393904c0f4e5b63769ff4b32ad24e
Aleksey <aleksey.vahoninnd@gmail.com>
2023-08-04 12:10:29 +0300

    task036

---
+++ task036/Program.cs
@@ -0,0 +1,45 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. 
+// Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+
+    return arr;
+}
+
+void PrintArray(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write($"{arr[i]}, ");
+        else Console.Write($"{arr[i]}");
+    }
+    Console.Write("]");
+}
+
+int SumNonEvenPositionArray(int[] arr)
+{
+    int result = 0;
+    for (int i = 1; i < arr.Length; i += 2)
+    {
+        result += arr[i];
+    }
+    return result;
+}
+
+int[] array = CreateArrayRndInt(5, -99, 100);
+int sumNonEvenPositionArray = SumNonEvenPositionArray(array);
+
+PrintArray(array);
+Console.Write($" -> {sumNonEvenPositionArray}");
+Console.WriteLine();
